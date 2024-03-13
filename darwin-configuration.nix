17afe9b8af1f97123f6649101f4a627a4a6fa4ce
raf <rraf@tuta.io>
2024-03-13 22:17:46 +0000

    update: migrated firefox config from SHAGOHOD

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -13,7 +13,13 @@ in
   };
 
   home-manager.users.raf = { pkgs, ... }: {
-    nixpkgs.config.allowUnfree = true;
+    nixpkgs.config = {
+      allowUnfree = true;
+      packageOverrides = pkgs: {
+        nur = import (fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") { inherit pkgs; };
+        unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") { };
+      };
+    };
     home.stateVersion = "23.11";
     home.packages = with pkgs; [
       raycast
@@ -50,6 +56,52 @@ in
       enable = true;
       userName = "raf";
       userEmail = "rraf@tuta.io";
+    };
+    programs.firefox = {
+      enable = true;
+      package = null;
+      profiles = {
+        default = {
+          isDefault = true;
+          extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+            ublock-origin
+            vimium
+            sponsorblock
+            youtube-recommended-videos
+          ];
+          userChrome = ''
+            #TabsToolbar { visibility: collapse !important; }
+            #main-window:not([customizing]) #navigator-toolbox:not(:focus-within):not(:hover){
+                margin-top: -45px;
+            }
+          '';
+          settings = {
+            "browser.startup.homepage" = "https://nixos.org";
+            "browser.search.region" = "GB";
+            "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+            "app.normandy.first_run" = false;
+            "app.shield.optoutstudies.enabled" = false;
+            "app.update.channel" = "default";
+            "browser.urlbar.quickactions.enabled" = false;
+            "browser.urlbar.quickactions.showPrefs" = false;
+            "browser.urlbar.shortcuts.quickactions" = false;
+            "browser.urlbar.suggest.quickactions" = false;
+            "dom.forms.autocomplete.formatautofill" = false;
+            "extensions.update.enabled" = false;
+            "extensions.webcompat.enable_picture_in_picture_override" = true;
+            "extensions.webcompat.enable_shims" = true;
+            "extensions.webcompat.perform_injections" = true;
+            "extensions.webcompat.perform_ua_overrides" = true;
+            "privacy.donottrackheader.enabled" = true;
+            "signon.rememberSignons" = false;
+            "browser.formfill.enable" = false;
+            "signon.autofillForms" = false;
+            "extensions.formautofill.addresses.enabled" = false;
+            "extensions.formautofill.creditCards.enabled" = false;
+            "extensions.formautofill.hueristics.enabled" = false;
+          };
+        };
+      };
     };
   };
 
@@ -140,6 +192,7 @@ in
     enable = true;
     casks = [
       "karabiner-elements"
+      "firefox"
     ];
   };
 }
